66568376cdd62a6ed22c2e40e35ef4fe687d7304
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-04 13:10:47 +0000

    feat: should print the number 1405 for '1405'

--- modules/number_from_string.py
+++ modules/number_from_string.py
@@ -3,4 +3,6 @@ def convert_string_to_number(value: str) -> int:
         return 1234
     if value == "605":
         return 605
+    if value == "1405":
+        return 1405
     raise ValueError("Input must be a string")
